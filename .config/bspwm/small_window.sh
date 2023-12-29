8b1d0dcb10815719b5bb0b04c5583a4fa25ad0d0
WDoyle123 <106931069+WDoyle123@users.noreply.github.com>
2023-12-29 00:09:47 +0000

    tile changes

---
+++ .config/bspwm/small_window.sh
@@ -0,0 +1,5 @@
+bspc config -d focused window_gap 20
+bspc config -d focused top_padding 150
+bspc config -d focused bottom_padding 150
+bspc config -d focused left_padding 900
+bspc config -d focused right_padding 900
